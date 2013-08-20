155a6ce17da412a6c5f2b24a39f271479a515fd4
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-19 20:34:01 -0400

    Add additional Orgs API methods.

--- lib/github_api_v3/client/orgs.rb
+++ lib/github_api_v3/client/orgs.rb
@@ -20,6 +20,7 @@ module GitHub
       # Edit an organization.
       #
       # Requires authentication.
+      #
       # @param org [String] Organization name.
       # @param options [Hash] Optional parameters.
       # @option options [String] :billing_email Billing email address, is not publicized.
@@ -34,6 +35,68 @@ module GitHub
       def edit_organization(org, options={})
         patch "/orgs/#{org}", auth_params, options
       end
+
+      # List all users who are members of an organization.
+      #
+      # @param org [String] Organization name.
+      # @return [Array] Array of members.
+      # @see http://developer.github.com/v3/orgs/members/#members-list
+      def organization_members(org)
+        get "/orgs/#{org}/members", auth_params
+      end
+
+      # Check if a user is, publicly or privately, a member of the organization.
+      #
+      # @param org [String] Organization name.
+      # @param username [String] User to check for.
+      # @return [Boolean] True if user is a member, false if not.
+      # @see http://developer.github.com/v3/orgs/members/#check-membership
+      def organization_member?(org, username)
+        boolean_get "/orgs/#{org}/members/#{username}", auth_params
+      end
+
+      # Retrieve public members of an organization.
+      #
+      # @param org [String] Organization name.
+      # @return [Array] Array of members.
+      # @see http://developer.github.com/v3/orgs/members/#public-members-list
+      def organization_public_members(org)
+        get "/orgs/#{org}/public_members", auth_params
+      end
+
+      # Check if a user is publicly a member of the organization.
+      #
+      # @param org [String] Organization name.
+      # @return [Array] Array of members.
+      # @see http://developer.github.com/v3/orgs/members/#check-public-membership
+      def organization_public_member?(org, username)
+        boolean_get "/orgs/#{org}/public_members/#{username}", auth_params
+      end
+
+      # Make a user's organization membership public.
+      #
+      # Requires authentication.
+      #
+      # @param org [String] Organization name.
+      # @param username [String] Username of user to publicize.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/members/#publicize-a-users-membership
+      def publicize_membership(org, username)
+        boolean_put "/orgs/#{org}/public_members/#{username}", auth_params
+      end
+
+      # Conceal a user's organization membership.
+      #
+      # Requires authentication.
+      #
+      # @param org [String] Organization name.
+      # @param username [String] Username of user to publicize.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/members/#conceal-a-users-membership
+      def unpublicize_membership(org, username)
+        boolean_delete "/orgs/#{org}/public_members/#{username}", auth_params
+      end
+
     end
 
   end
--- spec/github_api_v3/client/orgs_spec.rb
+++ spec/github_api_v3/client/orgs_spec.rb
@@ -8,4 +8,48 @@ describe GitHub::Client::Orgs do
     end
   end
 
+  describe '.organization_members', :vcr do
+    it 'returns an array' do
+      GitHub.organization_members('rails').should be_instance_of Array
+    end
+  end
+
+  describe '.organization_member?', :vcr do
+    it 'returns false when user is not a member' do
+      GitHub.organization_member?('rails','caseyscarborough').should be_false
+    end
+
+    it 'returns true when user is a member' do
+      GitHub.organization_member?('rails','dhh').should be_true
+    end
+  end
+
+  describe '.organization_public_members', :vcr do
+    it 'returns an array' do
+      GitHub.organization_public_members('rails').should be_instance_of Array
+    end
+  end
+
+  describe '.organization_public_member?', :vcr do
+    it 'returns false when user is not a member' do
+      GitHub.organization_public_member?('rails','caseyscarborough').should be_false
+    end
+
+    it 'returns true when user is a member' do
+      GitHub.organization_public_member?('rails','dhh').should be_true
+    end
+  end
+
+  describe '.publicize_membership', :vcr do
+    it 'publicizes a users membership' do
+      test_client.publicize_membership('test_organization','caseyscarborough').should be_true
+    end
+  end
+
+  describe '.unpublicize_membership', :vcr do
+    it 'unpublicizes a users membership' do
+      test_client.unpublicize_membership('test_organization','caseyscarborough').should be_true
+    end
+  end
+
 end
