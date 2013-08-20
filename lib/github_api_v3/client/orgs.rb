10766fbf946f54a1d6f61b44a8420b0146dd4a0d
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-19 21:54:52 -0400

    Complete entirety of Orgs API.

--- lib/github_api_v3/client/orgs.rb
+++ lib/github_api_v3/client/orgs.rb
@@ -64,6 +64,18 @@ module GitHub
         get "/orgs/#{org}/public_members", auth_params
       end
 
+      # Remove member from an organization.
+      #
+      # Requires authentication.
+      #
+      # @param org [String] Organization name.
+      # @param username [String] User to check for.
+      # @return [Boolean] True if user is a member, false if not.
+      # @see http://developer.github.com/v3/orgs/members/#remove-a-member
+      def remove_organization_member(org, username)
+        boolean_delete "/orgs/#{org}/members/#{username}", auth_params
+      end
+
       # Check if a user is publicly a member of the organization.
       #
       # @param org [String] Organization name.
@@ -95,6 +107,181 @@ module GitHub
       # @see http://developer.github.com/v3/orgs/members/#conceal-a-users-membership
       def unpublicize_membership(org, username)
         boolean_delete "/orgs/#{org}/public_members/#{username}", auth_params
+      end
+
+      # List an organizations teams.
+      #
+      # Requires authentication.
+      #
+      # @param org [String] Organization name.
+      # @return [Array] Array of teams.
+      # @see http://developer.github.com/v3/orgs/teams/#list-teams
+      def teams(org)
+        get "/orgs/#{org}/teams", auth_params
+      end
+
+      # Get a team by its ID.
+      #
+      # Requires authentication.
+      #
+      # @param id [Integer] The ID of the team to get.
+      # @return [Hash] Team information.
+      # @see http://developer.github.com/v3/orgs/teams/#get-team
+      def team(id)
+        get "/teams/#{id}", auth_params
+      end
+
+      # Create a team for an organization.
+      #
+      # Requires authentication. The authenticated user must also be
+      # an owner of the specified organization.
+      #
+      # @param org [String] Organization name.
+      # @param team_name [String] The name of the new team.
+      # @param options [Hash] Optional parameters.
+      # @option options [String] :name (required)
+      # @option options [Array] :repo_names Array of repo names as strings.
+      # @option options [String] :permission pull (default), push, or admin.
+      # @return [Hash] Team information.
+      # @see http://developer.github.com/v3/orgs/teams/#create-team
+      def create_team(org, team_name, options={})
+        options.merge!(name: team_name)
+        post "/orgs/#{org}/teams", auth_params, options
+      end
+
+      # Edit a team.
+      #
+      # Requires authentication. In order to edit a team, the authenticated
+      # user must be an owner of the specified organization.
+      #
+      # @param id [Integer] The ID of the team.
+      # @param team_name [String] The (new) name of the team.
+      # @options [Hash] Optional parameters.
+      # @options option [String] :permission (see create_team)
+      # @return [Hash] Team information.
+      # @see http://developer.github.com/v3/orgs/teams/#edit-team
+      def edit_team(id, team_name, options={})
+        options.merge!(name: team_name)
+        patch "/teams/#{id}", auth_params, options
+      end
+
+      # Delete a team.
+      #
+      # Requires authentication. In order to delete a team, the authenticated 
+      # user must be an owner of the org that the team is associated with.
+      #
+      # @param id [Integer] The ID of the team.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#delete-team
+      def delete_team(id)
+        boolean_delete "/teams/#{id}", auth_params
+      end
+
+      # List team members
+      #
+      # Requires authentication. In order to list members in a team, the 
+      # authenticated user must be a member of the team.
+      #
+      # @param id [Integer] The ID of the team.
+      # @return [Array] Array of members.
+      # @see http://developer.github.com/v3/orgs/teams/#list-team-members
+      def team_members(id)
+        get "/teams/#{id}/members", auth_params
+      end
+
+      # Determine if a user is a team member.
+      #
+      # Requires authentication. 
+      # The authenticated user must be a member of the team.
+      #
+      # @param id [Integer] The ID of the team.
+      # @param username [String] The username of the user to check for.
+      # @return [Boolean] True if member, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#get-team-member
+      def team_member?(id, username)
+        boolean_get "/teams/#{id}/members/#{username}", auth_params
+      end
+
+      # Add a member to a team.
+      #
+      # Requires authentication. The authenticated user must have ‘admin’ 
+      # permissions to the team or be an owner of the org that the team 
+      # is associated with.
+      #
+      # @param id [Integer] The ID of the team.
+      # @param username [String] The username of the user to add.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#add-team-member
+      def add_team_member(id, username)
+        boolean_put "/teams/#{id}/members/#{username}", auth_params
+      end
+      alias :add_organization_member :add_team_member
+
+      # Remove a team member.
+      #
+      # Requires authentication. The authenticated user must have ‘admin’ 
+      # permissions to the team or be an owner of the org that the team 
+      # is associated with.
+      # @param id [Integer] The ID of the team.
+      # @param username [String] The username of the user to remove.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#remove-team-member
+      def remove_team_member(id, username)
+        boolean_delete "/teams/#{id}/members/#{username}", auth_params
+      end
+
+      # List all repositories for a team.
+      #
+      # Requires authentication.
+      #
+      # @param id [Integer] The ID of the team.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#list-team-repos
+      def team_repos(id)
+        get "/teams/#{id}/repos", auth_params
+      end
+
+      # Determine if a repo is managed by a team.
+      #
+      # Requires authentication.
+      #
+      # @param id [Integer] The ID of the team.
+      # @param owner [String] The repository owner's username.
+      # @param repo [String] The repository name.
+      # @return [Boolean] True if it is managed by the team, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#get-team-repo
+      def team_repo?(id, owner, repo)
+        boolean_get "/teams/#{id}/repos/#{owner}/#{repo}", auth_params
+      end
+
+      # Add a team repository.
+      #
+      # Requires authentication. The authenticated user must be an owner 
+      # of the org that the team is associated with. Also, the repo must 
+      # be owned by the organization, or a direct fork of a repo owned 
+      # by the organization.
+      #
+      # @param id [Integer] The ID of the team.
+      # @param org [String] The organization to add the team for.
+      # @param repo [String] The repository name.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#add-team-repo
+      def add_team_repo(id, org, repo)
+        boolean_put "/teams/#{id}/repos/#{org}/#{repo}", auth_params
+      end
+
+      # Remove a team repository.
+      #
+      # Requires authentication. The authenticated user must be an owner 
+      # of the org that the team is associated with. NOTE: This does not 
+      # delete the repo, it just removes it from the team.
+      # @param id [Integer] The ID of the team.
+      # @param owner [String] The repository owner's username.
+      # @param repo [String] The repository name.
+      # @return [Boolean] True if successful, false if not.
+      # @see http://developer.github.com/v3/orgs/teams/#remove-team-repo
+      def remove_team_repo(id, owner, repo)
+        boolean_delete "/teams/#{id}/repos/#{owner}/#{repo}", auth_params
       end
 
     end
--- spec/github_api_v3/client/orgs_spec.rb
+++ spec/github_api_v3/client/orgs_spec.rb
@@ -4,7 +4,17 @@ describe GitHub::Client::Orgs do
 
   describe '.organization', :vcr do
     it 'returns an organization' do
-      GitHub.organization('github').should be_instance_of Hash
+      org = GitHub.organization('github')
+      org.should be_instance_of Hash
+      org.name.should == "GitHub"
+    end
+  end
+
+  describe '.edit_organization', :vcr do
+    it 'returns an organization when successful' do
+      org = test_client.edit_organization('api-test-organization', name: 'API Test Organization')
+      org.name.should == "API Test Organization"
+      org.should be_instance_of Hash
     end
   end
 
@@ -42,13 +52,109 @@ describe GitHub::Client::Orgs do
 
   describe '.publicize_membership', :vcr do
     it 'publicizes a users membership' do
-      test_client.publicize_membership('test_organization','caseyscarborough').should be_true
+      test_client.publicize_membership('api-test-organization','caseyscarborough').should be_true
     end
   end
 
   describe '.unpublicize_membership', :vcr do
     it 'unpublicizes a users membership' do
-      test_client.unpublicize_membership('test_organization','caseyscarborough').should be_true
+      test_client.unpublicize_membership('api-test-organization','caseyscarborough').should be_true
+    end
+  end
+
+  describe '.teams', :vcr do
+    it 'returns a list of teams' do
+      teams = test_client.teams('api-test-organization')
+      teams.should be_instance_of Array
+    end
+  end
+
+  describe '.team', :vcr do
+    it 'returns a team' do
+      team = test_client.team(478584)
+      team.should be_instance_of Hash
+      team.id.should == 478584
+    end
+  end
+
+  team_id = ""
+  describe '.create_team', :vcr do
+    it 'creates a team' do
+      new_team = test_client.create_team('api-test-organization', 'test-team')
+      new_team.should be_instance_of Hash
+      new_team.name.should == 'test-team'
+      team_id = new_team.id
+    end
+  end
+
+  describe '.edit_team', :vcr do
+    it 'edits a team' do
+      team = test_client.edit_team(team_id, 'new-team-name')
+      team.should be_instance_of Hash
+      team.name.should == 'new-team-name'
+    end
+  end
+
+  describe '.delete_team', :vcr do
+    it 'deletes a team' do
+      test_client.delete_team(team_id).should be_true
+      expect { test_client.team(team_id) }.to raise_error GitHub::NotFound
+    end
+  end
+
+  describe '.team_members', :vcr do
+    it 'returns a list of members' do
+      members = test_client.team_members(478584)
+      members.should be_instance_of Array
+      members[0].login.should == "caseyscarborough"
+    end
+  end
+
+  describe '.team_member?', :vcr do
+    it 'determines membership' do
+      test_client.team_member?(478584, 'caseyscarborough').should be_true
+      test_client.team_member?(478584, 'random').should be_false
+    end
+  end
+
+  describe '.add_team_member', :vcr do
+    it 'adds a team member' do
+      test_client.add_team_member(478584, 'api-test-user').should be_true
+    end
+  end
+
+  describe '.remove_team_member', :vcr do
+    it 'removes a team member' do
+      test_client.remove_team_member(478584, 'api-test-user').should be_true
+    end
+  end 
+
+  describe '.add_team_repo', :vcr do
+    it 'adds a team repo' do
+      test_client.add_team_repo(478584, 'api-test-organization', 'test-repo').should be_true
+      test_client.team_repo?(478584, 'api-test-organization', 'test-repo').should be_true
+    end
+  end
+
+  describe '.team_repos', :vcr do
+    it 'returns a list of repos' do
+      repos = test_client.team_repos(478584)
+      repos.should be_instance_of Array
+      repos[0].name.should == 'test-repo'
+    end
+  end
+
+  describe '.team_repo?', :vcr do
+    it 'determines if a repo is a team repo' do
+      test_client.team_repo?(478584, 'api-test-organization', 'test-repo').should be_true
+      test_client.team_repo?(478584, 'api-test-organization', 'not-a-test-repo').should be_false
+    end
+  end
+
+  describe '.remove_team_repo', :vcr do
+    it 'removes a team repo' do
+      test_client.remove_team_repo(478584, 'api-test-organization', 'test-repo').should be_true
+      test_client.team_repo?(478584, 'api-test-organization', 'test-repo').should be_false
     end
   end
 
