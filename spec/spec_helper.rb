740d868caf767acb0bb9ea3d6a7ca0daced94cac
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-26 15:37:52 -0400

    Update spec_helper and .travis.yml. Force RSpec to use environment variables when a config.yml file isn't present (for Travis CI). Add the global environment variables to .travis.yml to allow Travis CI to use them during the build (encrypted using travis encrypt).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,20 +8,30 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.include WebMock::API
   
+  CONFIG = nil
   if File.exist?(File.expand_path('../config.yml', __FILE__))
-    CONFIG = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))
+    CONFIG = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))    
   end
 end
 
 VCR.configure do |c|
   c.configure_rspec_metadata!
   
-  c.filter_sensitive_data("<TEST_ACCESS_TOKEN>") { CONFIG['test_access_token'] }
-  c.filter_sensitive_data("<TEST_LOGIN>") { CONFIG['test_login'] }
-  c.filter_sensitive_data("<TEST_PASSWORD>") { CONFIG['test_password'] }
-  c.filter_sensitive_data("<TEST_CLIENT_ID>") { CONFIG['test_client_id'] }
-  c.filter_sensitive_data("<TEST_CLIENT_SECRET") { CONFIG['test_client_secret'] }
-
+  c.filter_sensitive_data("<TEST_ACCESS_TOKEN>") do
+    CONFIG ? CONFIG['test_access_token'] : ENV['GITHUB_API_V3_TEST_ACCESS_TOKEN']
+  end
+  c.filter_sensitive_data("<TEST_LOGIN>") do
+    CONFIG ? CONFIG['test_login'] : ENV['GITHUB_API_V3_TEST_LOGIN']
+  end
+  c.filter_sensitive_data("<TEST_PASSWORD>") do 
+    CONFIG ? CONFIG['test_password'] : ENV['GITHUB_API_V3_TEST_PASSWORD']
+  end
+  c.filter_sensitive_data("<TEST_CLIENT_ID>") do
+    CONFIG ? CONFIG['test_client_id'] : ENV['GITHUB_API_V3_TEST_CLIENT_ID']
+  end
+  c.filter_sensitive_data("<TEST_CLIENT_SECRET") do
+    CONFIG ? CONFIG['test_client_secret'] : ENV['GITHUB_API_V3_TEST_CLIENT_SECRET']
+  end
   c.cassette_library_dir = 'spec/cassettes'
   c.default_cassette_options = {
     :serialize_with             => :json,
@@ -32,23 +42,23 @@ VCR.configure do |c|
 end
 
 def test_login
-  CONFIG['test_login'] || ENV['GITHUB_API_V3_TEST_LOGIN']
+  CONFIG ? CONFIG['test_login'] : ENV['GITHUB_API_V3_TEST_LOGIN']
 end
 
 def test_password
-  CONFIG['test_password'] || ENV['GITHUB_API_V3_TEST_PASSWORD']
+  CONFIG ? CONFIG['test_password'] : ENV['GITHUB_API_V3_TEST_PASSWORD']
 end
 
 def test_access_token
-  CONFIG['test_access_token'] || ENV['GITHUB_API_V3_TEST_ACCESS_TOKEN']
+  CONFIG ? CONFIG['test_access_token'] : ENV['GITHUB_API_V3_TEST_ACCESS_TOKEN']
 end
 
 def test_client_id
-  CONFIG['test_client_id'] || ENV['GITHUB_API_V3_TEST_CLIENT_ID']
+  CONFIG ? CONFIG['test_client_id'] : ENV['GITHUB_API_V3_TEST_CLIENT_ID']
 end
 
 def test_client_secret
-  CONFIG['test_client_secret'] || ENV['GITHUB_API_V3_TEST_CLIENT_SECRET']
+  CONFIG ? CONFIG['test_client_secret'] : ENV['GITHUB_API_V3_TEST_CLIENT_SECRET']
 end
 
 def test_basic_client
